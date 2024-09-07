46bb58254ed6a412bb1b8ae2daed1d693525aab1
amit712singhal <rakshit.singhal712@gmail.com>
2024-09-07 17:50:55 +0530

    chore: Update fcfs algorithm

--- Code/prg_3_fcfs.c
+++ Code/prg_3_fcfs.c
@@ -1,100 +1,113 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX 100
 
 typedef struct
 {
-  int pid;
-  int burst_time;
-  int waiting_time;
-  int turnaround_time;
+  int pid;        // Process ID
+  int arrival;    // Arrival time
+  int burst;      // Burst time
+  int completion; // Completion time
+  int waiting;    // Waiting time
+  int turnaround; // Turnaround time
 } Process;
 
-void print_table(Process p[], int n);
-void print_gantt_chart(Process p[], int n);
+// Function to sort processes by arrival time
+void sortByArrival(Process *p, int n)
+{
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = 0; j < n - i - 1; j++)
+    {
+      if (p[j].arrival > p[j + 1].arrival)
+      {
+        Process temp = p[j];
+        p[j] = p[j + 1];
+        p[j + 1] = temp;
+      }
+    }
+  }
+}
+
+// Main FCFS logic
+void fcfsScheduling(Process *p, int n)
+{
+  int time = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    if (time < p[i].arrival)
+      time = p[i].arrival; // Set time to the process arrival time if idle
+
+    time += p[i].burst;
+    p[i].completion = time;
+    p[i].turnaround = p[i].completion - p[i].arrival;
+    p[i].waiting = p[i].turnaround - p[i].burst;
+  }
+}
+
+// Function to display the Gantt chart
+void displayGanttChart(Process *p, int n)
+{
+  int startTime = p[0].arrival;
+  printf("Gantt Chart:\n%d", startTime);
+
+  for (int i = 0; i < n; i++)
+  {
+    printf(" -- P%d -- %d", p[i].pid, p[i].completion);
+  }
+
+  printf("\n\n");
+}
+
+// Function to calculate and display average times
+void calculateAverages(Process *p, int n)
+{
+  float totalTurnaround = 0, totalWaiting = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    totalTurnaround += p[i].turnaround;
+    totalWaiting += p[i].waiting;
+  }
+
+  printf("\nAverage Turnaround Time: %.2f\n", totalTurnaround / n);
+  printf("Average Waiting Time: %.2f\n", totalWaiting / n);
+}
+
+// Function to display process information
+void displayResults(Process *p, int n)
+{
+  printf("PID\tArrival\t Burst\t Completion\tTurnaround\tWaiting\n");
+  for (int i = 0; i < n; i++)
+  {
+    printf("%d\t%d\t %d\t %d\t\t%d\t\t%d\n", p[i].pid, p[i].arrival, p[i].burst,
+           p[i].completion, p[i].turnaround, p[i].waiting);
+  }
+}
 
 int main()
 {
-  Process p[MAX];
-  int i, j, n;
-  int sum_waiting_time = 0, sum_turnaround_time = 0;
-
-  printf("\n5C6 - Amit Singhal (11614802722) \n");
-
-  printf("\nEnter total number of processes: ");
+  int n;
+  printf("\n5C6 - Amit Singhal (11614802722)\n");
+  printf("\nEnter number of processes: ");
   scanf("%d", &n);
+  Process p[n];
 
-  printf("\nEnter burst time for each process:\n");
-  for (i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
   {
+    printf("\nEnter Arrival Time and Burst Time for Process %d: ", i + 1);
     p[i].pid = i + 1;
-    printf("P[%d]: ", i + 1);
-    scanf("%d", &p[i].burst_time);
-    p[i].waiting_time = 0;
-    p[i].turnaround_time = 0;
+    scanf("%d%d", &p[i].arrival, &p[i].burst);
+    p[i].completion = 0; // Initially, no process is completed
   }
+  printf("\n");
 
-  p[0].turnaround_time = p[0].burst_time;
-  for (i = 1; i < n; i++)
-  {
-    p[i].waiting_time = p[i - 1].waiting_time + p[i - 1].burst_time;
-    p[i].turnaround_time = p[i].waiting_time + p[i].burst_time;
-  }
+  sortByArrival(p, n);
+  fcfsScheduling(p, n);
+  displayGanttChart(p, n);
+  displayResults(p, n);
+  calculateAverages(p, n);
 
-  for (i = 0; i < n; i++)
-  {
-    sum_waiting_time += p[i].waiting_time;
-    sum_turnaround_time += p[i].turnaround_time;
-  }
-
-  puts("");
-  print_table(p, n);
-  printf("\nTotal Waiting Time: %d\n", sum_waiting_time);
-  printf("Average Waiting Time: %.2lf\n", (double)sum_waiting_time / (double)n);
-  printf("Total Turnaround Time: %d\n", sum_turnaround_time);
-  printf("Average Turnaround Time: %.2lf\n", (double)sum_turnaround_time / (double)n);
-
-  puts("\nGANTT CHART\n");
-  print_gantt_chart(p, n);
+  printf("\n");
 
   return 0;
-}
-
-void print_table(Process p[], int n)
-{
-  int i;
-
-  puts("+-----+------------+--------------+-----------------+");
-  puts("| PID | Burst Time | Waiting Time | Turnaround Time |");
-  puts("+-----+------------+--------------+-----------------+");
-
-  for (i = 0; i < n; i++)
-  {
-    printf("| %3d | %10d | %12d | %15d |\n", p[i].pid, p[i].burst_time, p[i].waiting_time, p[i].turnaround_time);
-    puts("+-----+------------+--------------+-----------------+");
-  }
-}
-
-void print_gantt_chart(Process p[], int n)
-{
-  int i, j;
-
-  // Top border of the Gantt chart
-  puts("+----+----+----+----+----+----+");
-
-  // Process IDs
-  puts("| P1 | P2 | P3 | P4 | P5 | P6 |");
-
-  // Bottom border of the Gantt chart
-  puts("+----+----+----+----+----+----+");
-
-  for (i = 0; i < n; i++)
-  {
-    printf("| %d  ", p[i].turnaround_time);
-    if (p[i].turnaround_time > 9)
-      printf("\b"); // Remove 1 space if the number has 2 digits
-  }
-  printf("|\n");
-  puts("+----+----+----+----+----+----+");
-  printf("\n");
 }
